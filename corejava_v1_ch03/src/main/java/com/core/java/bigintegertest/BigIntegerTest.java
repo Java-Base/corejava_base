c5c64f3a0e6023741c76c35518ea720e058c438f
macmini4 <macmini4@XingGudeMac-mini-4.local>
2016-08-02 20:16:26 +0800

    init core java project

---
+++ corejava_v1_ch03/src/main/java/com/core/java/bigintegertest/BigIntegerTest.java
@@ -0,0 +1,11 @@
+package com.core.java.bigintegertest;
+
+/**
+ * This program uses big numbers to compute the odds of winning the grand prize in a lottery
+ *
+ * @author 	Lian
+ * @time	2016年8月2日
+ */
+public class BigIntegerTest {
+
+}
---
+++ corejava_v1_ch03/src/main/java/com/core/java/firstsample/FirstSample.java
@@ -0,0 +1,15 @@
+package com.core.java.firstsample;
+
+/**
+ * This is the first sample program in Core Java Chapter 3
+ *
+ * @author 	Lian
+ * @time	2016年8月2日
+ *
+ */
+public class FirstSample {
+
+	public static void main(String[] args) {
+		System.out.println("We will not use 'Hello, World!'");
+	}
+}
