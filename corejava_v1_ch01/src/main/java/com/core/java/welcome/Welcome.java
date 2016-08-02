2a423d8d7dc4282738bb9edbbe68f517fb217285
macmini4 <macmini4@XingGudeMac-mini-4.local>
2016-08-02 20:13:34 +0800

    init core java project

---
+++ corejava_v1_ch01/src/main/java/com/core/java/welcome/Welcome.java
@@ -0,0 +1,22 @@
+package com.core.java.welcome;
+
+/**
+ * This program displays a greeting from the authors
+ *
+ * @author 	Lian
+ * @time	2016年8月2日
+ *
+ */
+public class Welcome {
+
+	public static void main(String[] args) {
+		String[] greeting = new String[3];
+		greeting[0] = "Welcom to Core Java";
+		greeting[1] = "by Cay Horstmann";
+		greeting[2] = "and Gary Cornell";
+
+		for(String str : greeting) {
+			System.out.println(str);
+		}
+	}
+}
