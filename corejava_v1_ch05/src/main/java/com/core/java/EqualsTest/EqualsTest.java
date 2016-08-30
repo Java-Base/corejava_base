7d2a0537e91375f4451e13e56f4fd7d2fe54a92c
lianxinzhong <macmini4@XingGudeMac-mini-4.local>
2016-08-30 17:34:53 +0800

    add equals, hashCode, toString method example

--- corejava_v1_ch05/src/main/java/com/core/java/EqualsTest/EqualsTest.java
+++ corejava_v1_ch05/src/main/java/com/core/java/EqualsTest/EqualsTest.java
@@ -28,6 +28,7 @@ public class EqualsTest {
 		Manager carl = new Manager("Carl Cracker", 80000, 1987, 12, 15);
 		Manager boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
 		boss.setBonus(5000);
+
 		System.out.println("boss.toString(): " + boss);
 		System.out.println("carl.equals(boss): " + carl.equals(boss));
 		System.out.println("alice1.hashCode(): " + alice1.hashCode());
