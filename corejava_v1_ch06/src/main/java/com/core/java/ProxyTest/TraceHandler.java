559b213fb6e6d3978902d14fc24e7ce9665ceca6
lianxinzhong <macmini4@XingGudeMac-mini-4.local>
2016-09-07 17:45:33 +0800

    add ProxyTest project

---
+++ corejava_v1_ch06/src/main/java/com/core/java/ProxyTest/TraceHandler.java
@@ -0,0 +1,50 @@
+package com.core.java.ProxyTest;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * An invocation handler that prints out the method name and parameters,
+ * then invoke the original method
+ *
+ * @auth Lian
+ * @date 16/9/7
+ */
+class TraceHandler implements InvocationHandler {
+
+	private Object target;
+
+	/**
+	 * Constructs a TraceHandler
+	 *
+	 * @param obj	the implicit parameter of the mothod call
+	 */
+	public TraceHandler(Object obj) {
+		this.target = obj;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+		// print implicit argument
+		System.out.print(target);
+
+		// print method name
+		System.out.print("." + method.getName() + "(");
+
+		// print explicit arguments
+		if (args != null) {
+			for (int i = 0; i < args.length; i++) {
+				System.out.print(args[i]);
+				if(i<args.length -1) {
+					System.out.print(", ");
+				}
+			}
+		}
+
+		System.out.println(")");
+
+		// invoke actual method
+		return method.invoke(target, args);
+	}
+}
