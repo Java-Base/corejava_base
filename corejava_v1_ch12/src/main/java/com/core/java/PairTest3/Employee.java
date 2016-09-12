aec0f3571d32f90d7d158a571115baf4d37dcac6
lianxinzhong <macmini4@XingGudeMac-mini-4.local>
2016-09-12 14:17:17 +0800

    add Generic prigramming projects

---
+++ corejava_v1_ch12/src/main/java/com/core/java/PairTest3/Employee.java
@@ -0,0 +1,39 @@
+package com.core.java.PairTest3;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * @auth Lian
+ * @date 16/9/12
+ */
+class Employee {
+
+	private String name;
+	private double salary;
+	private Date hireDay;
+
+	public Employee(String name, double salary, int year, int month, int day) {
+		this.name = name;
+		this.salary = salary;
+		GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+		this.hireDay = calendar.getTime();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public Date getHireDay() {
+		return hireDay;
+	}
+
+	public void raiseSalary(double byPercent) {
+		double raise = salary * byPercent / 100;
+		salary += raise;
+	}
+}
